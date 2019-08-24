a0e4de461ad4f5c32665abd78970d6cc59b00c7f
wh810807... <1484099238@qq.com>
2019-08-24 11:49:48 +0800

    springboot入门案例

---
+++ src/main/java/com/wuheng/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.wuheng.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+	
+
+}
